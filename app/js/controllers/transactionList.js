7c78f83b4e8dc1bf86f9ee6afb8d88442830579a
Nicolas Bonamy <nicolas@bonamy.fr>
2013-05-23 16:54:08 +0200

    Added custom validation messages and table sorting

--- app/js/controllers/transactionList.js
+++ app/js/controllers/transactionList.js
@@ -1,6 +1,8 @@
 
 app.controller('TransactionListController', function($scope, TransactionResource) {
 	
+	$scope.sortOrder = 'instant';
+	
 	$scope.transactions = TransactionResource.query({}, function() {
 	}, function() {
 		app.setError('An error occured while retrieving the transaction list');
